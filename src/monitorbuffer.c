81ed26efc7b4cec8810bfbf75dff2c6cdc7790f3
Daniele Tricoli <eriol@mornie.org>
2011-12-13 23:07:03 +0100

    Added a monitorbuffer ADT

---
+++ src/monitorbuffer.c
@@ -0,0 +1,58 @@
+#include <pthread.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "monitorbuffer.h"
+
+int buffer_init(buffer_t *b, int size)
+{
+    b->count = b->number_of_readers = 0;
+    b->size = size;
+    b->data = (buf_element_t *) malloc(sizeof(buf_element_t) * size);
+    pthread_mutex_init(&b->mtx_write);
+    pthread_mutex_init(&b->mtx_read);
+}
+
+int buffer_append(buffer_t *b, int value)
+{
+    pthread_mutex_lock(&b->mtx_write);
+    b->data[b->count] = value;
+    b->count++;
+    pthread_mutex_unlock(&b->mtx_write);
+}
+
+int buffer_read(buffer_t *b, buf_element_t *dest)
+{
+    int read_emelents = 0
+    pthread_mutex_lock(&b->mtx_read);
+    b->number_of_readers++;
+    if (b->number_of_readers == 1)
+        pthread_mutex_lock(&b->mtx_write);
+    pthread_mutex_unlock(&b->mtx_read);
+
+    // TODO: use a condition
+    if (b->count > 0) {
+        memcpy(dest, b->data, b->count);
+        read_emelents = b->count;
+    }
+
+    pthread_mutex_lock(&b->mtx_read);
+    b->number_of_readers--;
+    if (b->number_of_readers == 0)
+        pthread_mutex_unlock(&b->mtx_write);
+    pthread_mutex_unlock(&b->mtx_read);
+
+    return read_emelents;
+}
+
+int buffer_clean(buffer_t *b)
+{
+    pthread_mutex_lock(&b->mtx_write);
+    b->count = 0;
+}
+
+int buffer_free(buffer_t *b)
+{
+    if (b->data != NULL)
+        free(b->data);
+}
---
+++ src/monitorbuffer.h
@@ -0,0 +1,24 @@
+#ifndef MONITORBUFFER_H
+#define MONITORBUFFER_H
+
+#include <pthread.h>
+
+#define buf_element_t int
+
+typedef struct {
+    int count, size, number_of_readers;
+    buf_element_t *data;
+    pthread_mutex_t mtx_write, mtx_read;
+} buffer_t;
+
+int buffer_init(buffer_t *b, int size);
+
+int buffer_append(buffer_t *b, int value);
+
+int buffer_get_actual_data_size(buffer_t *b);
+
+int buffer_read(buffer_t *b, int value);
+
+int buffer_clean(buffer_t *b);
+
+#endif /* MONITORBUFFER_H */
