089f32c0bbc94431ff12871b7bca39e8cabde7cd
Daniele Tricoli <eriol@mornie.org>
2011-12-14 06:51:13 +0100

    Updated to use thread safe buffer

--- src/main.c
+++ src/main.c
@@ -4,14 +4,21 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <unistd.h>
+
+#include "monitorbuffer.h"
 #include "utils.h"
 
-char ch = 0;
+
+
+buffer_t input_buffer;
 
 void * input_thread(void *arg)
 {
+    char ch = 0;
     while(true) {
         ch = getch();
+        buffer_append(&input_buffer, ch);
     }
     return 0;
 }
@@ -19,9 +26,12 @@ void * input_thread(void *arg)
 int main(void)
 {
     pthread_t thr;
-    int s;
+    int s, res;
     unsigned short row, col;
-    
+    buf_element_t local_buffer[BUFFER_MAX_SIZE];
+
+    buffer_init(&input_buffer, BUFFER_MAX_SIZE);
+
     s = pthread_create(&thr, NULL, input_thread, NULL);
     if (s != 0) {
         perror("Error in pthread_create.");
@@ -31,14 +41,13 @@ int main(void)
     printf("Message from main()\n");
     printf("row: %hd col %hd\n", row, col);
 
-//     while (true) {
-//         printf("%c\n", ch);
-//         if (ch == 113) { // q pressed
-//             break;
-//         } else {
-//             continue;
-//         }
-//     }
+    while (true) {
+        res = buffer_read(&input_buffer, local_buffer);
+        if (res > 0) {
+            printf("%d\n", atoi(local_buffer));
+            printf("%d\n", res);
+        }
+    }
 
     exit(EXIT_SUCCESS);
 }
