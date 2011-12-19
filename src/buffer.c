e3c0ed5b56c2441a6eae5329690ed3f7e01299ac
Daniele Tricoli <eriol@mornie.org>
2011-12-20 00:30:28 +0100

    Refactored to use the object pattern

--- src/buffer.c
+++ src/buffer.c
@@ -11,15 +11,27 @@ typedef struct buffer_rw_t {
 };
 
 
-int buffer_rw_init(BufferRW b, int size)
+int buffer_rw_create(int size)
 {
+    BufferRW b = malloc(sizeof(struct buffer_rw_t));
+    if (b == NULL) {
+        perror("buffer_rw_create: can't create BufferRW");
+        return NULL;
+    }
+
     b->count = b->number_of_readers = 0;
     b->size = size;
+
     b->data = (buf_element_t *) malloc(sizeof(buf_element_t) * size);
+    if (b->data == NULL) {
+        perror("buffer_rw_create: can't allocate memory for buffer data");
+        return NULL;
+    }
+
     pthread_mutex_init(&b->mtx_write, NULL);
     pthread_mutex_init(&b->mtx_read, NULL);
 
-    return (b->data == NULL) ? 1 : 0;
+    return b;
 }
 
 void buffer_rw_append(BufferRW b, buf_element_t value)
@@ -33,6 +45,7 @@ void buffer_rw_append(BufferRW b, buf_element_t value)
 int buffer_rw_read(BufferRW b, buf_element_t *dest)
 {
     int read_elements = 0;
+
     pthread_mutex_lock(&b->mtx_read);
     b->number_of_readers++;
     if (b->number_of_readers == 1)
@@ -64,4 +77,6 @@ void buffer_rw_free(BufferRW b)
 {
     if (b->data != NULL)
         free(b->data);
+
+    free(b);
 }
