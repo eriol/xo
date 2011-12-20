85df03cef8a7222b193fcffb69cc128dc44089ed
Daniele Tricoli <eriol@mornie.org>
2011-12-20 02:38:24 +0100

    Added game_controller thread

--- src/main.c
+++ src/main.c
@@ -6,67 +6,88 @@
 #include <string.h>
 #include <unistd.h>
 
-
 #include "canvas.h"
-#include "monitorbuffer.h"
+#include "buffer.h"
 #include "terminal.h"
 #include "xo.h"
 
-buffer_t input_buffer;
+#define BUFFER_RW_MAX_SIZE 10
 
-void * input_thread(void *arg)
-{
-    char ch = 0;
-    while(true) {
-        ch = getch();
-        buffer_append(&input_buffer, ch);
-    }
-    return 0;
-}
+void *input_controller(void *arg);
+void *game_controller(void *arg);
+
+BufferRW input_buffer;
+Canvas canvas, collision;
 
 int main(void)
 {
     setlocale(LC_ALL, "");
-    Canvas canvas, collision;
-    pthread_t thr;
-    int s, res, inserted_creature[] = {0, 0};
+    int s;
+    pthread_t thr_input_controller, thr_game_controller;
     unsigned short rows, cols;
-    buf_element_t local_buffer[BUFFER_MAX_SIZE];
+    buffer_element_t local_buffer[BUFFER_RW_MAX_SIZE];
 
-    buffer_init(&input_buffer, BUFFER_MAX_SIZE);
+    input_buffer = buffer_rw_create(BUFFER_RW_MAX_SIZE);
 
     // Initialize random seed
     srandom(time(NULL));
 
-//     s = pthread_create(&thr, NULL, input_thread, NULL);
-//     if (s != 0) {
-//         perror("Error in pthread_create.");
-//     }
-
     terminal_get_size(&rows, &cols);
     canvas = canvas_create(rows, cols, true);
     collision = canvas_create(rows, cols, false);
-
     xo_intro(canvas);
-    xo_draw_game_layout(canvas, collision);
-    xo_draw_creature(canvas, collision, 1, 1, true, (int []) {NONE, F_RED, NONE});
-    xo_draw_bunch_creatures(canvas, collision, true, 4);
-    xo_draw_creature_random_point(canvas, collision, true,
-                                  (int []) {NONE, F_GREEN, NONE});
-    xo_draw_random_creatures(canvas, collision, 20, inserted_creature);
-    xo_draw_the_chosen_one(canvas, collision, true);
-    xo_draw_life(canvas, 3);
-    xo_draw_timebar100(canvas, 50, NULL);
 
-
-    canvas_draw(canvas);
-//     sleep(1);
-//     canvas_draw(collision);
+    s = pthread_create(&thr_input_controller, NULL, input_controller, NULL);
+    if (s != 0) {
+        perror("pthread_create: failed to create thr_input_controller");
+    }
+    s = pthread_create(&thr_game_controller, NULL, game_controller, NULL);
+    if (s != 0) {
+        perror("pthread_create: failed to create thr_game_controller");
+    }
 
     while (true) {
-        sleep(10);
+        sleep(1);
     }
 
     canvas_destroy(canvas);
     exit(EXIT_SUCCESS);
+}
+
+void *input_controller(void *arg)
+{
+    char ch = 0;
+
+    while(true) {
+        ch = getch();
+
+        if (ch >= '0' && ch <='9') {
+            buffer_rw_append(input_buffer, ch);
+        } else if (ch == ' ') {
+            buffer_rw_clean(input_buffer);
+        }
+    }
+
+    return 0;
+}
+
+void *game_controller(void *arg)
+{
+    int inserted_number = 0;
+    int s, res, inserted_creature[] = {0, 0};
+
+    while(true) {
+        xo_draw_game_layout(canvas, collision);
+        //     xo_draw_creature(canvas, collision, 1, 1, true, (int []) {NONE, F_RED, NONE});
+        //     xo_draw_bunch_creatures(canvas, collision, true, 4);
+        //     xo_draw_creature_random_point(canvas, collision, true,
+        //                                   (int []) {NONE, F_GREEN, NONE});
+        //     xo_draw_random_creatures(canvas, collision, 20, inserted_creature);
+        //     xo_draw_the_chosen_one(canvas, collision, true);
+        //     xo_draw_life(canvas, 3);
+        //     xo_draw_timebar100(canvas, 50, NULL);
+        canvas_draw(canvas);
+        sleep(10);
+    }
+
 }
