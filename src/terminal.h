372126958e83167008cb105ebd9f9a6f0896616b
Daniele Tricoli <eriol@mornie.org>
2011-12-20 06:13:36 +0100

    Fixed data type

--- src/terminal.h
+++ src/terminal.h
@@ -3,9 +3,9 @@
 
 char getch(void);
 void terminal_clear(void);
-void terminal_draw_horizontal_line(int x, int y, int lenght, char ch);
-void terminal_draw_vertical_line(int x, int y, int lenght, char ch);
+void terminal_draw_horizontal_line(int x, int y, int lenght, wchar_t ch);
+void terminal_draw_vertical_line(int x, int y, int lenght, wchar_t ch);
 int terminal_get_size(unsigned short *row, unsigned short *col);
-void terminal_put_chat_at(int x, int y, char ch);
+void terminal_put_chat_at(int x, int y, wchar_t ch);
 
-#endif /* TERMINAL_H */
+#endif
